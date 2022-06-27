337ff060dad71bd6f3762adc6bfd8453a62d6dd3
Dev Singh Kanyal <77870205+Mr-Dev-S-K@users.noreply.github.com>
2022-06-27 14:17:32 +0530

    updated on 27 June 2022

---
+++ .history/Molecular Dynamics Simulation of Hard Spheres/particle_collision_main_20220627123545.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.9.12 ('base')",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "ec7997cbd54d87edba654026e2a652b93bc6051da1342c7fe6c251ffda352ebe"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
